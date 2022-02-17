ecb41a59f1afd671e376b51d190b8f7e8a89d794
marc durand <mjm.durand@hotmail.fr>
2022-02-17 17:39:07 +0100

    putting new iptables in log file

--- build-on-rpi.sh
+++ build-on-rpi.sh
@@ -108,8 +108,8 @@ echo -e "\e[1;32mSources successfully downloaded.\e[0m"
 
 #Port Forwarding
 echo -e "\n\e[1;44mPort Forwarding (80 > 1337).\e[0m"
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true &>> ${LOG} | sudo debconf-set-selections &>> ${LOG}
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true &>> ${LOG} | sudo debconf-set-selections &>> ${LOG}
 sudo apt install -y iptables-persistent &>> ${LOG}
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 1337
 
