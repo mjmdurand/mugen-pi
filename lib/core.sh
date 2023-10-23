a2206cf9bcb3221cc9695e33f1142126ea6bf823
mjmdurand <mjm.durand@hotmail.fr>
2023-10-24 00:09:55 +0200

    make nodejs install quiet

--- lib/core.sh
+++ lib/core.sh
@@ -12,7 +12,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs -yq
 
 sudo apt install -yq curl nodejs mpv ffmpeg postgresql libpq-dev postgresql-client postgresql-client-common git &>> ${LOG}
 sudo npm install -g yarn &>> ${LOG}
