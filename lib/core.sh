87cc91c474b8495bdc77a1fae6c0c7103e9b77e8
mjmdurand <mjm.durand@hotmail.fr>
2023-10-24 00:33:47 +0200

    removing duplicate install and redirect output to log file

--- lib/core.sh
+++ lib/core.sh
@@ -6,24 +6,21 @@ echo -e "\e[1;32mSystem updated.\e[0m"
 
 echo -e "\n\e[1;44mInstalling required software.\e[0m"
 # adding nodejs 18.x repositories
-sudo apt install -yq ca-certificates curl gnupg
+sudo apt install -yq ca-certificates curl gnupg &> ${LOG}
 
 if [ -f "/etc/apt/keyrings/nodesource.gpg" ]; then
     echo "nodesource.gpg exists, skipping key installation."
 else
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    sudo mkdir -p /etc/apt/keyrings &> ${LOG}
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key &> ${LOG} | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg &> ${LOG}
 fi
 
-
-
-
 NODE_MAJOR=18
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt install -yq nodejs 
-sudo apt install -yq npm 
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" &> ${LOG}| sudo tee /etc/apt/sources.list.d/nodesource.list &> ${LOG}
+sudo apt install -yq nodejs &> ${LOG}
+sudo apt install -yq npm &> ${LOG}
 
-sudo apt install -yq curl nodejs mpv ffmpeg postgresql libpq-dev postgresql-client postgresql-client-common git &>> ${LOG}
+sudo apt install -yq mpv ffmpeg postgresql libpq-dev postgresql-client postgresql-client-common git &>> ${LOG}
 sudo npm install -g yarn &>> ${LOG}
 sudo apt autoremove -yq &>> ${LOG}
 echo -e "\e[1;32mRequired software installation done.\e[0m"
